16776a3c3512ab01d4f3c383e20c6626ffc8cd12
charliefea <charlotte.feather@yahoo.com>
2017-06-28 16:24:24 +0100

    Associated the users with posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ApplicationRecord
   has_many :comments
+  belongs_to :user
+
 
   def time_format
     created_at.to_formatted_s(:short)
   end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627154253) do
+ActiveRecord::Schema.define(version: 20170628143122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170627154253) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 20170627154253) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "posts", "users"
 end
