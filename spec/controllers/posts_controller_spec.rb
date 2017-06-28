78ce69b44bb04ad586d9ef635859f89917f78df4
charliefea <charlotte.feather@yahoo.com>
2017-06-28 18:05:33 +0100

    Added models and corrected tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+require "support/features/clearance_helpers"
 
 RSpec.describe PostsController, type: :controller do
+
   describe "GET /new " do
     it "responds with 200" do
       sign_in
@@ -11,11 +13,13 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      post :create, params: { post: { message: "Hello, world!"} }
+      sign_in
+      post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
 
     it "creates a post" do
+      sign_in
       post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
--- spec/features/posts_contains_author_spec.rb
+++ spec/features/posts_contains_author_spec.rb
@@ -4,9 +4,8 @@ RSpec.feature 'Posts', type: :feature do
   scenario "Posts show email" do
     user = User.create({email: "test@mail.com", password: "password"})
     post = Post.create(message: "Test Post", user_id: user.id)
-    p user
-    p post
+
     visit posts_url(as: user)
-    expect('div#posts').to have_content("test@mail.com")
+    expect('#posts ul li:first-child').to have_content("test@mail.com")
   end
 end
