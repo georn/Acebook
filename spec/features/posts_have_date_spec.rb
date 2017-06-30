4c7ba2dd45fc24941b69affae367b4eaf8159718
y0m0 <lixi.daniele@gmail.com>
2017-06-30 23:23:33 +0100

    Fixes tests

--- spec/features/posts_have_date_spec.rb
+++ spec/features/posts_have_date_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature 'Post', type: :feature do
-  let(:comment) { create(:post, message: 'old post') }
-  let(:new_comment) { create(:post, message: 'new post') }
 
   scenario 'Can submit post and display the date' do
     user = User.create({email: "test@mail.com", password: "password"})
     Post.create(message: "Test Post", user_id: user.id)
-    visit "/posts"
+
+    visit posts_url(as: user)
     date = Post.find_by(message: "Test Post").created_at
     expect(page).to have_content(date.to_formatted_s(:short))
   end
@@ -17,8 +16,7 @@ RSpec.feature 'Post', type: :feature do
     Post.create(message: "Old Post", user_id: user.id)
     Post.create(message: "New Post", user_id: user.id)
 
-
-    visit "/posts"
+    visit posts_url(as: user)
     expect(page.find('.post:first-child .post_body')).to have_content "New Post"
   end
 end
